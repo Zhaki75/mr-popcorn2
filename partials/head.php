13eb2912eb9bf7dff2e8e93930fd0da6a6daf34e
El ouarroudi <zhaki.elouarroudi@gmail.com>
2025-12-23 23:00:28 +0100

    Morcelement de l'application

---
+++ partials/head.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title><?= isset($title) && !empty($title) ? htmlspecialchars($title) : 'Cinema' ?> - Mr Popcorn</title>
+
+        <!-- Seo -->
+         <meta name="robots" content="index, follow">
+         <meta name="descrition" content="<?= isset($description) && !empty($description) ? htmlspecialchars($title) : 'Cinema' ?>">
+         <meta name="author" content="Mr Popcorn">
+         <meta name="publisher" content="Dwwm22">
+         <meta name="keywords" content="<?= isset($keywords) && !empty($keywords) ? htmlspecialchars($title) : 'Cinema' ?>">
+
+        <!-- Favicon -->
+         <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+
+        <!-- Font awesome -->
+         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+        <!-- Google Font -->
+         <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+
+        <!-- Boostrap Css-->
+         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+
+        <!-- My custom StyleSheet-->
+        <link rel="stylesheet" href="/styles/app.css">
+
+    </head>
+    <body class="bg-light">
---
+++ public/create.php
@@ -0,0 +1,21 @@
+     <?php
+    $title = "Nouveau film";
+    $description= "Ajout d'un nouveau film...";
+    $keywords = "Cinéma, répertoire, film, dwwm";
+    ?>
+    
+    
+    <?php include_once __DIR__ . "/../partials/head.php"; ?>
+
+    <?php include_once __DIR__ . "/../partials/nav.php"; ?>
+
+        <!-- Main -->
+         <main class="container">
+            <h1 class="text-center my-3 display-5">Nouveau film</h1>
+
+            <!-- Formulaire des nouveau films -->
+         </main>
+    
+    <?php include_once __DIR__ . "/../partials/footer.php"; ?>
+
+    <?php include_once __DIR__ . "/../partials/foot.php"; ?>
