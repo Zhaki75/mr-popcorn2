7c5259b578a847b4acf4c9022c06d4476526f525
El ouarroudi <zhaki.elouarroudi@gmail.com>
2025-12-25 00:33:44 +0100

    Création du formulaire d'ajout d'un nouveau film en base de données

--- public/create.php
+++ public/create.php
@@ -1,4 +1,5 @@
-     <?php
+
+   <?php
     $title = "Nouveau film";
     $description= "Ajout d'un nouveau film...";
     $keywords = "Cinéma, répertoire, film, dwwm";
@@ -14,6 +15,28 @@
             <h1 class="text-center my-3 display-5">Nouveau film</h1>
 
             <!-- Formulaire des nouveau films -->
+             <div class="container">
+               <div class="col-md-8 col-lg-5 mx-auto p-4 bg-white shadow rounded">
+                  <form method="post">
+                     <div class="mb-3">
+                        <label for="title">Titre <span class="text-danger">*</label>
+                        <input type="text" name="title" id="title" class="form-control" autofocus>
+                     </div>
+                     <div class="mb-3">
+                        <label for="title">Note / 5</label>
+                        <input type="number" min="0" max="5" step=".5" inputmode="decimal" name="rating" id="title" class="form-control" autofocus>
+                     </div>
+                     <div class="mb-3">
+                        <label for="comment">Laissez un commentaire</label>
+                        <textarea name="comment" id="comment" class="form-control"></textarea>
+
+                     </div>
+                     <div>
+                        <input type="submit" value="Ajouter" class="w-100 btn btn-primary shadow">
+                     </div>
+                  </form>
+               </div>
+             </div>
          </main>
     
     <?php include_once __DIR__ . "/../partials/footer.php"; ?>
